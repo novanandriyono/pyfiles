4455c4b23aedfbab7f8d56a20982dcc8467ada97
Novan Andriyono <evelynradtya@gmail.com>
2018-01-14 16:17:56 +0700

    update test

--- test.js
+++ test.js
@@ -1,10 +1,10 @@
 var path = require('path');
 var pyfiles = require('./index.js');
 var except1 = __dirname + path.sep + 'index.js';
-var except2 = __dirname + path.sep + 'test.js';
+var except2 = __dirname + path.sep + '.git';
 var options = {
 	'fromDir': __dirname,
-	'formatFile': '*',
+	'formatFile': 'file',
 	'srcFiles': false,
 	'except': [except1,except2],
 }
