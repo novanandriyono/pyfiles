81ab95c4f8239e0147cd8594902b9fcc36471ed8
Novan Andriyono <evelynradtya@gmail.com>
2018-01-14 15:53:46 +0700

    test

--- test.js
+++ test.js
@@ -1,7 +1,11 @@
+var path = require('path');
 var pyfiles = require('./index.js');
+var except1 = __dirname + path.sep + 'index.js';
+var except2 = __dirname + path.sep + 'test.js';
 var options = {
 	'fromDir': __dirname,
-	'formatFile': 'js',
+	'formatFile': '*',
 	'srcFiles': false,
+	'except': [except1,except2],
 }
 console.log(pyfiles(options));
