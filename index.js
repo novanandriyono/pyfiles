f882963454c7251605232c6b884990ef138fc6cf
Novan Andriyono <evelynradtya@gmail.com>
2018-01-14 15:53:34 +0700

    add except

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ return pyfiles(options);
 };
 
 function pyfiles(options){
+var path = require('path');
 var fs = require('fs');
 var file = [];
 	if(typeof options !== 'object'){
@@ -17,26 +18,27 @@ var file = [];
 	var dir = options.fromDir;
 	var formatFile = options.formatFile;
 	var srcFiles = options.srcFiles;
+	var except = options.except;
 	if(fs.statSync(dir).isDirectory()){
 		if(formatFile === '*'){
-			results = fromDirAll(dir);
+			results = fromDirAll(dir, except);
 		}else if(formatFile === 'dir'){
-			results = justDir(dir);
+			results = justDir(dir, except);
 		}else if(srcFiles === true){
-			results = searchFile(dir, formatFile);
+			results = searchFile(dir, formatFile, except);
 		}else{
-			results = fromDir1(dir, formatFile);
+			results = fromDir1(dir, formatFile, except);
 		}
 	}
 
 
-	function fromDir1(dir,formatFile){
+	function fromDir1(dir,formatFile, except){
 		var dir1 = fs.readdirSync(dir);
 		for(var i = 0, len1 = dir1.length; i < len1; i++){
 			var dirstat1 = dir + '/' + dir1[i];
 			var parts = dirstat1.split('.');
 			var format = parts[parts.length - 1];
-			if((fs.statSync(dirstat1).isFile()) && (format == formatFile)){
+			if((fs.statSync(dirstat1).isFile()) && (format == formatFile) && (exceptThis(dirstat1,except) === true)){
 			  	file.push(dirstat1);
 			}else if(fs.statSync(dirstat1).isDirectory()){
 				fromDir1(dirstat1,formatFile)
@@ -47,13 +49,14 @@ var file = [];
 		return file;
 	}
 
-	function fromDirAll(dir,formatFile){
+	function fromDirAll(dir, except){
 		var dir1 = fs.readdirSync(dir);
 		for(var i = 0, len1 = dir1.length; i < len1; i++){
 			var dirstat1 = dir + '/' + dir1[i];
-			if(fs.statSync(dirstat1).isFile()){
+			if((fs.statSync(dirstat1).isFile()) && (exceptThis(dirstat1,except) === true)){
 			  	file.push(dirstat1);
-			}else if(fs.statSync(dirstat1).isDirectory()){
+			}else if(fs.statSync(dirstat1).isDirectory() && (exceptThis(dirstat1,except) === true)){
+				file.push(dirstat1);
 				fromDirAll(dirstat1);
 			}else{
 				// :(sad
@@ -62,11 +65,11 @@ var file = [];
 		return file;
 	}
 
-	function justDir(dir){
+	function justDir(dir, except){
 		var dir1 = fs.readdirSync(dir);
 		for(var i = 0, len1 = dir1.length; i < len1; i++){
 			var dirstat1 = dir + '/' + dir1[i];
-			if(fs.statSync(dirstat1).isDirectory()){
+			if((fs.statSync(dirstat1).isDirectory()) && (exceptThis(dirstat1,except) === true)){
 			  	file.push(dirstat1);
 			  	justDir(dirstat1);
 			}else{
@@ -76,11 +79,11 @@ var file = [];
 		return file;
 	}
 
-	function searchFile(dir,formatFile){
+	function searchFile(dir,formatFile, except){
 		var dir1 = fs.readdirSync(dir);
 		for(var i = 0, len1 = dir1.length; i < len1; i++){
 			var dirstat1 = dir + '/' + dir1[i];
-			if(dirstat1.indexOf(formatFile) !== -1){
+			if((dirstat1.indexOf(formatFile) !== -1) && (exceptThis(dirstat1,except) === true)){
 			  	file.push(dirstat1);
 			}else if(fs.statSync(dirstat1).isDirectory()){
 				searchFile(dirstat1,formatFile);
@@ -91,5 +94,21 @@ var file = [];
 		return file;
 	}
 
+	function exceptThis(line, except){
+		var results = true;
+		line = path.normalize(line);
+		if(typeof except === 'object'){
+			for(var eT = 0, len2 = except.length; eT < len2; eT++){
+				var fileExcept = path.normalize(except[eT]);
+				if(fileExcept.indexOf(line) !== -1){
+					results = false;
+				}
+			}
+		}else{
+			results = true;
+		}
+		return results;
+
+	}
 	return results;
 }
