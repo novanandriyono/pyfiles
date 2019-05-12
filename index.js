f74a7d83e34208be70a6f6fbf0f02407b490f3d1
novanandriyono <evelynradtya@gmail.com>
2019-05-13 02:44:43 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,132 +1,241 @@
 'use strict';
-module.exports = function(options){
-return pyfiles(options);
-};
+const fs = require('fs');
+const path = require('path');
 
-function pyfiles(options){
-var path = require('path');
-var fs = require('fs');
-var file = [];
-	if(typeof options !== 'object'){
-		return console.log('not valid options');
+module.exports = function(_s,_r=false, _t = 'N',_v = []){
+	return new pyfiles(_s,_r,_t,_v);
+}
+
+function pyfiles(_s,_r=false, _t = 'N',_v = []){
+	
+	fs.access(_s, (err) => {
+		if(err){
+			console.error('not have access');
+			return undefined;
+		}
+	});
+
+	if(!fs.statSync(_s).isDirectory()){
+		console.error('Not directory');
+		return undefined;
 	}
-	if((typeof options.fromDir == 'undefined')
-		&& (typeof options.formatFile == 'undefined')){
-		return options.formatFile || options.fromDir;
-	}
-	var results = undefined;
-	var dir = options.fromDir;
-	var formatFile = options.formatFile;
-	var srcFiles = options.srcFiles;
-	var except = options.except;
-	if(fs.statSync(dir).isDirectory()){
-		if(formatFile === '*'){
-			results = fromDirAll(dir, except);
-		}else if(formatFile === 'dir'){
-			results = justDir(dir, except);
-		}else if(formatFile === 'file'){
-			results = justFile(dir, except);
-		}else if(srcFiles === true){
-			results = searchFile(dir, formatFile, except);
-		}else{
-			results = fromDir1(dir, formatFile, except);
+
+	var i = 0;
+	var _vir = [];
+	/**
+	 * [_t type fillter]
+	 * @type {Array}
+	 */
+	_t = [
+	'N',
+	'include-equals',
+	'include-dir-equals',
+	'include-indexof',
+	'include-dir-indexof',
+	'include-indexof-first',
+	'include-dir-indexof-first',
+	'include-regex',
+	'exclude-equals',
+	'exclude-dir-equals',
+	'exclude-indexof',
+	'exclude-dir-indexof',
+	'exclude-indexof-first',
+	'exclude-dir-indexof-first',
+	'exclude-regex'
+	].includes(_t)?_t:'N'; 
+
+	if(_v.length > 0){
+		for (i = _v.length - 1; i >= 0; i--) {
+			_vir[i] = path.normalize(_s + path.sep + _v[i]);
 		}
 	}
 
+	var fsrs = fs.readdirSync(_s);
 
-	function fromDir1(dir,formatFile, except){
-		var dir1 = fs.readdirSync(dir);
-		for(var i = 0, len1 = dir1.length; i < len1; i++){
-			var dirstat1 = dir + '/' + dir1[i];
-			var parts = dirstat1.split('.');
-			var format = parts[parts.length - 1];
-			if((fs.statSync(dirstat1).isFile()) && (format == formatFile) && (exceptThis(dirstat1,except) === true)){
-			  	file.push(dirstat1);
-			}else if((fs.statSync(dirstat1).isDirectory()) && (exceptThis(dirstat1,except) === true)){
-				fromDir1(dirstat1,formatFile)
-			}else{
-				// :(sad
+	function getstat(str){
+		fs.access(str, (err) => {
+			if(err){
+				console.error("not have access to " + str);
+				return undefined;
 			}
-		}
-		return file;
+		});
+		return fs.statSync(str);
 	}
 
-	function fromDirAll(dir, except){
-		var dir1 = fs.readdirSync(dir);
-		for(var i = 0, len1 = dir1.length; i < len1; i++){
-			var dirstat1 = dir + '/' + dir1[i];
-			if((fs.statSync(dirstat1).isFile()) && (exceptThis(dirstat1,except) === true)){
-			  	file.push(dirstat1);
-			}
-			if(fs.statSync(dirstat1).isDirectory() && (exceptThis(dirstat1,except) === true)){
-				file.push(dirstat1);
-				fromDirAll(dirstat1,except);
-			}else{
-				// :(sad
-			}
+	function filters(str){
+		if(_t === 'N'){
+			return false;
 		}
-		return file;
+		if(_v.length > 0){
+			if(_v.length === 1){
+				return typeFiltersItem(str,0);
+			}
+			return typeFilters(str);
+		}
+		return false;	
 	}
 
-	function justDir(dir, except){
-		var dir1 = fs.readdirSync(dir);
-		for(var i = 0, len1 = dir1.length; i < len1; i++){
-			var dirstat1 = dir + '/' + dir1[i];
-			if((fs.statSync(dirstat1).isDirectory()) && (exceptThis(dirstat1,except) === true)){
-			  	file.push(dirstat1);
-			  	justDir(dirstat1);
-			}else{
-				// :(sad
-			}
-		}
-		return file;
-	}
-
-	function justFile(dir, except){
-		var dir1 = fs.readdirSync(dir);
-		for(var i = 0, len1 = dir1.length; i < len1; i++){
-			var dirstat1 = dir + '/' + dir1[i];
-			if((fs.statSync(dirstat1).isFile()) && (exceptThis(dirstat1,except) === true)){
-			  	file.push(dirstat1);
-			}else if((fs.statSync(dirstat1).isDirectory()) && (exceptThis(dirstat1,except) === true)){
-				justFile(dirstat1,except);
-			}else{
-				// :(sad
-			}
-		}
-		return file;
-	}
-
-	function searchFile(dir,formatFile, except){
-		var dir1 = fs.readdirSync(dir);
-		for(var i = 0, len1 = dir1.length; i < len1; i++){
-			var dirstat1 = dir + '/' + dir1[i];
-			if((dirstat1.indexOf(formatFile) !== -1) && (exceptThis(dirstat1,except) === true)){
-			  	file.push(dirstat1);
-			}else if((fs.statSync(dirstat1).isDirectory()) && (exceptThis(dirstat1,except) === true)){
-				searchFile(dirstat1,formatFile);
-			}else{
-				// :(sad
-			}
-		}
-		return file;
-	}
-
-	function exceptThis(line, except){
-		var results = true;
-		line = path.normalize(line);
-		if(typeof except === 'object'){
-			for(var eT = 0, len2 = except.length; eT < len2; eT++){
-				var fileExcept = path.normalize(except[eT]);
-				if(fileExcept.indexOf(line) !== -1){
-					results = false;
+	function typeFiltersItem(str,i = 0){
+			if(_t === 'include-equals'){
+				if(str === _v[i]){
+					return true;
 				}
+				return false;
 			}
-		}else{
-			results = true;
-		}
-		return results;
 
+			if(_t === 'include-dir-equals'){
+				if(str === _vir[i]){
+					return true;
+				}
+				return false;
+			}
+
+			if(_t === 'include-indexof'){
+				if(str.indexOf(_v[i]) !== -1){
+					return true;
+				}
+				return false;
+			}
+
+			if(_t === 'include-dir-indexof'){
+				if(str.indexOf(_vir[i]) !== -1){
+					return true;
+				}
+				return false;
+			}
+
+			if(_t === 'include-indexof-first'){
+				if(str.indexOf(_v[i],0) !== -1){
+					return true;
+				}
+				return false;
+			}
+
+			if(_t === 'include-dir-indexof-first'){
+				if(str.indexOf(_vir[i],0) !== -1){
+					return true;
+				}
+				return false;
+			}
+
+			// EX
+			if(_t === 'exclude-equals'){
+				if(str === _v[i]){
+					return true;
+				}
+				return false;
+			}
+
+			if(_t === 'exclude-dir-equals'){
+				if(str === _vir[i]){
+					return true;
+				}
+				return false;
+			}
+
+			if(_t === 'exclude-indexof'){
+				if(str.indexOf(_v[i]) !== -1){
+					return false;
+				}
+				return true;
+			}
+
+			if(_t === 'exclude-dir-indexof'){
+				if(str.indexOf(_vir[i]) !== -1){
+					return false;
+				}
+				return true;
+			}
+
+			if(_t === 'exclude-indexof-first'){
+				if(str.indexOf(_v[i],0) !== -1){
+					return false;
+				}
+				return true;
+			}
+
+			if(_t === 'exclude-dir-indexof-first'){
+				if(str.indexOf(_vir[i],0) !== -1){
+					return false;
+				}
+				return true;
+			}
+
+			var match = str.match(_v[i]);
+			
+			if(_t === 'include-regex'){
+				if(match !== null){
+					return true
+				}
+				return false;
+			}
+
+			if(_t === 'exclude-regex'){
+				if(match === null){
+					return true
+				}
+				return false;
+			}
+		console.error('unknow options filter');
+		return false;
 	}
-	return results;
+
+	
+
+	function typeFilters(str){
+		for (i = _v.length - 1; i >= 0; i--) {
+			if(typeFiltersItem(str,i)){
+				return true;
+			}
+		}
+		return false;
+	}
+
+	var all = [];
+	var files = [];
+	var dirs = [];
+	
+	for(i = fsrs.length - 1; i >= 0; i--) {
+		
+		fsrs[i] = path.normalize(_s + path.sep + fsrs[i]);
+		
+		var bool = filters(fsrs[i]);
+		
+		var stat = getstat(fsrs[i]);
+		
+		if(bool){
+			all.push(fsrs[i])
+		}
+
+		if(stat.isFile() && bool === true){
+			files.push(fsrs[i]);
+		}
+
+		if(stat.isDirectory() && bool === true){
+			dirs.push(fsrs[i]);
+		}
+		
+		if(_r===true && stat.isDirectory()){
+			var newi = new pyfiles(fsrs[i],_r,_t,_v);
+			Array.prototype.push.apply(files,newi.files);
+			Array.prototype.push.apply(dirs,newi.dirs);
+			Array.prototype.push.apply(all,newi.all);
+		}
+	}
+
+	Object.defineProperty(this, "files", {
+		value: files, 
+		writable: false 
+	});
+
+	Object.defineProperty(this, "dirs", {
+		value: dirs, 
+		writable: false 
+	});
+	
+	Object.defineProperty(this, "all", {
+		value: all, 
+		writable: false 
+	});
+	return this;
 }
